0a9022206cbe2957267763ad99eaebe756e19d35
Mmd_hushyari <mmd.hushyari0@gmail.com>
2024-03-12 07:08:47 -0700

    create product and category model and register admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . models import Product, Category, Image
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+    list_display = ('name', 'is_sub')
+    list_filter = ('is_sub', )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')
+    raw_id_fields = ('category',)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'product')
--- home/models.py
+++ home/models.py
@@ -1,15 +1,38 @@
+from tkinter import Image
+
 from django.db import models
 
 # Create your models here.
 
 
+class Category(models.Model):
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name="زیر مجموعه ",
+                                     related_name="subCategory", null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=30, verbose_name='نام دسته بندی ', unique=True)
+    slug = models.SlugField(max_length=30)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='دسته بندی ')
     name = models.CharField(max_length=25, verbose_name='نام محصول ')
     slug = models.SlugField(max_length=25, verbose_name='اسلاگ ', unique=True)
     description = models.TextField(verbose_name='توضیحات ')
     price = models.IntegerField(verbose_name='قیمت محصول ')
     available = models.BooleanField(default=True, verbose_name='موجودی محصول ')
-    created = models.DateTimeField(auto_now_add=True, verbose_name='')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'{self.name} - {self.created}'
 
 
 class Image(models.Model):
